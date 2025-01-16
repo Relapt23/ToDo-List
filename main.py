682627c0d228303cd1f9226ecb061c30da3bba99
Laptev Roman <romashlapt@mail.ru>
2025-01-16 19:32:44 +0700

    upd main

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi import FastAPI, status
 from fastapi.requests import Request
 from models import sessionmaker, create_engine, Base, ToDo, select, Tasks, InsertToDo, update, delete, CustomException
 from fastapi.templating import Jinja2Templates
 from typing import List
 
-engine = create_engine("sqlite:///database.db",echo=True)
+engine = create_engine("sqlite:///database.db", echo=True)
 sess = sessionmaker(engine)
 engine.echo = False
 Base.metadata.create_all(engine)
@@ -16,23 +15,22 @@ templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
+
 @app.get("/tasks", response_model=List[ToDo])
 async def get_tasks():
     with sess() as session:
         tasks = session.execute(select(Tasks)).scalars().all()
     return tasks
 
+
 @app.post("/tasks")
 async def add_task(task: InsertToDo):
     with sess() as session:
-        current_task = session.execute(select(Tasks).where(task.title == Tasks.title)).scalar_one_or_none()
-        if not current_task:
-            new_task = Tasks(title = task.title, description = task.description, status = 'todo')
-            session.add(new_task)
-            session.commit()
-            session.refresh(new_task)
-            return new_task
-        return {"message": "Успешное добавление"}
+        new_task = Tasks(title=task.title, description=task.description, status='todo')
+        session.add(new_task)
+        session.commit()
+    return new_task
+
 
 @app.get("/tasks/{id}/")
 async def get_current_task(id: int):
@@ -42,6 +40,7 @@ async def get_current_task(id: int):
             raise CustomException(detail="Задачи с таким id не найдено", status_code=404)
         return current_task
 
+
 @app.put("/tasks/{id}/")
 async def update_task(id: int, task: ToDo):
     with sess() as session:
@@ -49,18 +48,18 @@ async def update_task(id: int, task: ToDo):
         if not current_task:
             raise CustomException(detail="Задачи с таким id не найдено", status_code=404)
         session.execute(update(Tasks).where(id == Tasks.id).values(
-            title = task.title, description = task.description, status = task.status))
+            title=task.title, description=task.description, status=task.status))
         session.commit()
-    return {"message": "Информация обновлена"}
+        current_task = session.execute(select(Tasks).where(id == Tasks.id)).scalar_one_or_none()
+    return current_task
+
 
 @app.delete("/tasks/{id}/")
-async  def delete_task(id: int):
+async def delete_task(id: int):
     with sess() as session:
         current_task = session.execute(select(Tasks).where(id == Tasks.id)).scalar_one_or_none()
         if not current_task:
             raise CustomException(detail="Задачи с таким id не найдено", status_code=404)
         session.execute(delete(Tasks).where(id == Tasks.id))
         session.commit()
-    return {"message": "Успешное удаление"}
-
-
+    return current_task
